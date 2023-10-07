61970de2b1647a2a3b8732e9e89169b93d7b2030
Anil Piparaiya <78451912+anilpiparaiya@users.noreply.github.com>
2023-10-07 13:05:24 +0530

    Update Class.cpp

--- C++/Classes/Class.cpp
+++ C++/Classes/Class.cpp
@@ -2,48 +2,50 @@
 #include <sstream>
 using namespace std;
 class Student{
+        
+    private:
+        int age;
+        string first_name;
+        string last_name;
+        int standard;
+        
     public:
-    int get_age(){
-       return this->age;
+        int get_age(){
+        return this->age;
+        
+        }
+        void set_age(int age){
+            this->age=age;
+        }
+        
+        string get_first_name(){
+            return this->first_name;
+
+        }
+        void set_first_name(string first_name){
+            this->first_name = first_name;
+        }
+        string get_last_name(){
+            return this->last_name;
+        }
+        void set_last_name(string last_name){
+            this->last_name = last_name;
+        }
+        int get_standard(){
+            return this->standard;
+        }
+        void set_standard(int standard){
+            this->standard=standard;
+        } 
+        
+        string to_string(){
+            string fulldata;
+            fulldata = std::to_string(age)+"," +first_name +"," +last_name +","+ std::to_string(standard);
+            return fulldata;
+        }
+
     
-    }
-    void set_age(int age){
-        this->age=age;
-    }
-    string get_first_name(){
-        return this->first_name;
 
-    }
-    void set_first_name(string first_name){
-        this->first_name = first_name;
-
-
-    }
-    string get_last_name(){
-        return this->last_name;
-    }
-    void set_last_name(string last_name){
-        this->last_name = last_name;
-    }
-    int get_standard(){
-        return this->standard;
-    }
-    void set_standard(int standard){
-        this->standard=standard;
-    }
-    string to_string(){
-        string fulldata;
-        fulldata = std::to_string(age)+"," +first_name +"," +last_name +","+ std::to_string(standard);
-        return fulldata;
-    }
-
-    
-
-private:
-     int age;
-    string first_name;
-    string last_name;
-    int standard;
 };
 int main() {
     int age, standard;
