0085ade1806970df740759253c0de6e611e1204b
Anil Piparaiya <78451912+anilpiparaiya@users.noreply.github.com>
2023-11-28 22:12:05 +0530

    Create Is This a Binary Search Tree?.cpp

---
+++ Data Structures/Trees/Is This a Binary Search Tree?.cpp
@@ -0,0 +1,20 @@
+vector <int> v;       
+    bool checkBST(Node* root) 
+    {
+        if(root==NULL)
+        return true;
+        bool flag=checkBST(root->left);
+        if(!flag)
+            return false;
+        v.push_back(root->data);
+        flag=checkBST(root->right);
+        if(!flag)
+            return false;
+	//to check if array is Not in Ascending order
+        for(int i=0;i<v.size()-1;i++)     
+        {     
+            if(v[i]>=v[i+1])
+               return false;                    
+        }
+        return true;
+    }
